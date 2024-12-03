6c4ba0321a6b4b8facfefb275ab750052e77ef29
Schaffer, Carl <carl.schaffer@haufe-akademie.de>
2024-12-03 14:37:22 +0100

    add day 1 and day 3 of 2024

---
+++ 2024/day1.py
@@ -0,0 +1,45 @@
+from collections import Counter
+from helpers import read_input
+
+puzzle = read_input(1)
+
+
+sample = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+def parse_input(text):
+    a,b = [],[]
+    for line in text.split('\n'):
+        if len(line.strip()) < 1:
+            continue 
+        ai,bi = map(lambda x: int(x.strip()), line.split())
+        a.append(ai)
+        b.append(bi)
+    return a,b
+
+def solve1(text):
+    print("solving 1")
+    a,b = parse_input(text)
+    a = sorted(a)
+    b = sorted(b)
+    
+    res = []
+    for ai,bi in zip(a,b):
+        res.append(abs(ai-bi))
+    return sum(res)
+
+
+def solve2(text):
+    a,b = parse_input(text)
+    c = Counter(b)
+    # print(c)
+    return sum(map(lambda x: c.get(x,0)*x, a))
+
+print(solve1(sample))
+print(solve1(puzzle))
+print(solve2(sample))
+print(solve2(puzzle))
--- 2024/day3.py
+++ 2024/day3.py
@@ -1,7 +1,6 @@
-sample = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-
-
 import re
+
+sample = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
 
 pattern = re.compile(r"mul\((\d+),(\d+)\)")
