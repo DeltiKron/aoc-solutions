8276af6698b4cbdf6fe2f863c56de5eb7222fcd6
Schaffer, Carl <carl.schaffer@haufe-akademie.de>
2024-12-03 09:03:09 +0100

    solve 2022/5

---
+++ 2022/5/code.py
@@ -0,0 +1,33 @@
+import itertools
+from pathlib import Path
+
+
+def get_input(filename):
+    with open(Path(Path(__file__).parent, filename)) as f:
+        return f.read().strip().splitlines()
+
+
+def parse_input(lines):
+    split = lines.index("")
+    crates = lines[:split]
+    moves = lines[split + 1 :]
+    return crates, moves
+
+
+def parse_crates(lines):
+    lines == lines[::-1]
+    width = max(map(lambda x: len(x), lines))
+    for layer, l in enumerate(lines):
+        for i in range(1, min(width - 1, len(l)), 4):
+            print(layer, l[i], i)
+
+
+def solve1(inputfile):
+    lines = get_input(inputfile)
+    crates, moves = parse_input(lines)
+    parse_crates(crates)
+
+
+if __name__ == "__main__":
+    print(solve1("sample.txt"))
+    # print(solve2("puzzle.txt"))
