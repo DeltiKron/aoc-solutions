675f257a301a3ca4ca659c8542796f43b1bfeb16
Carl Schaffer <cfmschaffer@hotmail.de>
2024-12-17 23:01:29 +0100

    add ruff

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 mkdir -p $1/$2
 cd $1/$2
 
+
 echo 'from helpers import read_input' > code.py
 echo >> code.py
 echo 'sample=""""""' >> code.py
@@ -13,6 +14,5 @@ echo 'def solve1(puzzle):' >> code.py
 echo '    pass' >> code.py
 echo >> code.py
 echo 'print(solve1(sample))' >> code.py
-ruff --fix code.py
+ruff format code.py
 touch input_$2.txt
-
